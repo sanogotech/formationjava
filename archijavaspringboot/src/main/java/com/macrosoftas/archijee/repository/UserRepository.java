e9b37077818203c81aea9c0564ba06d683ac986d
Souleymane  SANOGO <souleymane.sanogo@macrosoftas.com>
2019-06-06 09:41:20 +0000

    Add new project full spring boot / security

---
+++ archijavaspringboot/src/main/java/com/macrosoftas/archijee/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.macrosoftas.archijee.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.macrosoftas.archijee.model.User;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Long> {
+	 User findByEmail(String email);
+}
